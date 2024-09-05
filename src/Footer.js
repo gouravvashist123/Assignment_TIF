d5128d0c4fc1e88c61747ef4a4a3d4d8828f1bf7
gouravvashist123 <gouravvashist1607@gmail.com>
2024-09-05 14:36:57 +0530

    Folder structure

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+       <p>Contact Us</p>
+      <ul>
+        <li>Lorem Ipsum Pvt Ltd.5/1, Magalton Road, Phartosh Gate near YTM Market, XYZ-343434</li>
+        <li>example2020@gmail.com</li>
+        <li>(904) 443-0343</li>
+      </ul>
+      <p>More</p>
+      <ul>
+         <li>About Us</li>
+         <li>Products</li>
+         <li>Career</li>
+         <li>Contact Us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Landing.js
@@ -0,0 +1,13 @@
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="landing">
+      <p>Discover the Best Food and Drinks</p>
+      <p>Naturally made Healthcare Products for the better care & support of your body.</p>
+      <p>Explore Now!</p>
+    </div>
+  );
+}
+
+export default Landing;
